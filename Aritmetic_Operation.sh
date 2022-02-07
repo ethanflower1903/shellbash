44682e3f080b412e05fb755741c08f84969b3332
ethan flower <84658436+ethanflower1903@users.noreply.github.com>
2022-02-07 00:18:30 -0800

    Create Aritmetic_Operation.sh

---
+++ Aritmetic_Operation.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+num1=20
+num2=5
+
+echo $(( num1 + num2 ))
+echo $(( num1 - num2 ))
+echo $(( num1 * num2 ))
+echo $(( num1 / num2 ))
+echo $(( num1 % num2 ))
+
+#alternate way
+
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )   #in * we have to use \ char. with *
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
