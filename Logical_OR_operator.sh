9de2f8ccf08d275bfd97afe383cfc067bb1453b6
ethan flower <84658436+ethanflower1903@users.noreply.github.com>
2022-02-07 00:17:32 -0800

    Create Logical_OR_operator.sh

---
+++ Logical_OR_operator.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+age=60
+
+# if [ "$age" -gt 18 -o "$age" -lt 30 ]  alternate choise of and opr. 
+# if [[ "$age" -gt 18 || "$age" -lt 30 ]]  alternate choise of and opr. 
+
+
+if [ "$age" -gt 18 ] || [ "$age" -lt 30 ]
+then
+    echo "valid age"
+    else
+    echo "age not valid"
+fi
