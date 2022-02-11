b804ca2410a8eef9ee6597108ee1b6c65577f31f
ethan flower <84658436+ethanflower1903@users.noreply.github.com>
2022-02-11 00:10:49 -0800

    Create Array_Variables.sh

---
+++ Array_Variables.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+os=('ubuntu' 'windows' 'ios' 'kali')
+os[0]="mac" #add or update element
+unset os[3] # remove the element
+
+echo "${os[0]}"
+echo "${os[1]}"
+echo "${os[2]}"
+echo "${os[@]}" # @ isareti ile hepsini yazdiriyoruz.
+echo "${!os[@]}" # !os[@] ile liestnin indexini yazdiriyoruz.
+echo "${#os[@]}" # ile listenin eleman sayisini yazdiriyoruz
+
+
+string=asfasfasdfasdfasdf
+echo "${string[@]}"
+echo "${string[0]}"
+echo "${string[1]
