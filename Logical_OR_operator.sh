e0405d7d28eb10cd828ea19717ddcfdbdd498ad5
Ethan Flower <84658436+ethanflower1903@users.noreply.github.com>
2022-04-19 00:59:21 -0700

    Update Logical_OR_operator.sh

--- Logical_OR_operator.sh
+++ Logical_OR_operator.sh
@@ -3,7 +3,7 @@ age=60
 
 # if [ "$age" -gt 18 -o "$age" -lt 30 ]  alternate choise of and opr. 
 # if [[ "$age" -gt 18 || "$age" -lt 30 ]]  alternate choise of and opr. 
-
+# if important
 
 if [ "$age" -gt 18 ] || [ "$age" -lt 30 ]
 then
