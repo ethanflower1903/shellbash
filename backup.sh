407718712085163a6c53730648ebc3687902f2c2
Ethan Flower <84658436+ethanflower1903@users.noreply.github.com>
2022-04-19 01:06:45 -0700

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This bash script is used to backup a user's home directory to /tmp/.
+
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
