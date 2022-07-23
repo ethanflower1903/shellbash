dfffbf535dcba50c9642e07db691cb1b6d077aa6
Ethan Flower-01100101 01110100 01101000 01100001 01101110 <84658436+ethanflower1903@users.noreply.github.com>
2022-07-22 23:44:58 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,5 +9,4 @@ output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 tar -czf $output $input
 echo "Backup of $input completed! Details about the output backup file:"
 ls -l $output
-#important
-#  
+#important...
